e5def890944bc4d31d834bef96b8275e5cc7e6ff
Christian Bernet <bernet@vitmain2.ch>
2018-07-11 16:59:04 +0200

    added description and new attributes

--- marugoto-backend/src/main/java/ch/uzh/marugoto/backend/data/entity/Page.java
+++ marugoto-backend/src/main/java/ch/uzh/marugoto/backend/data/entity/Page.java
@@ -5,35 +5,62 @@ import org.springframework.data.annotation.Id;
 import com.arangodb.springframework.annotation.Document;
 import com.arangodb.springframework.annotation.HashIndex;
 
+/**
+ * business model page
+ * 
+ * @author Christian
+ */
+
 @Document("pages")
 @HashIndex(fields = { "title" }, unique = true)
 public class Page {
 
 	@Id
 	private String id;
-
 	private String title;
 	private boolean isActive;
+	private Integer timeLimit;
+	private boolean isTimerVisible;
+	private boolean isEndOfStory;
+	private boolean isNotebookOpen;
+	private boolean autoTransitionOnTimeExpiration;
 
 	public String getId() {
 		return id;
 	}
 
-	public Page() {
-		super();
+	public String getTitle() {
+		return title;
+	}
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public Integer getTimeLimit() {
+		return timeLimit;
+	}
+
+	public boolean isTimerVisible() {
+		return isTimerVisible;
+	}
+
+	public boolean isEndOfStory() {
+		return isEndOfStory;
+	}
+
+	public boolean isNotebookOpen() {
+		return isNotebookOpen;
+	}
+
+	public boolean autoTransitionOnTimeExpiration() {
+		return autoTransitionOnTimeExpiration;
 	}
 
 	public Page(final String title, final boolean isActive) {
 		super();
 		this.title = title;
 		this.isActive = isActive;
-	}
-
-	// getter & setter
-
-	@Override
-	public String toString() {
-		return "Page [id=" + id + ", title=" + title + ", isActive=" + isActive + "]";
 	}
 
 }
--- marugoto-backend/src/main/java/ch/uzh/marugoto/backend/data/repository/PageRepository.java
+++ marugoto-backend/src/main/java/ch/uzh/marugoto/backend/data/repository/PageRepository.java
@@ -4,6 +4,12 @@ import com.arangodb.springframework.repository.ArangoRepository;
 
 import ch.uzh.marugoto.backend.data.entity.Page;
  
+/**
+ * repository page
+ * 
+ * @author Christian
+ */
+
 public interface PageRepository extends ArangoRepository<Page> {
  
 }
