c2af07de9263acd3991405db31943c63c4a58da5
Christian Bernet <bernet@vitmain2.ch>
2018-08-08 16:38:14 +0200

    added json for getpages

--- marugoto-backend/src/main/java/ch/uzh/marugoto/backend/controller/PageController.java
+++ marugoto-backend/src/main/java/ch/uzh/marugoto/backend/controller/PageController.java
@@ -4,22 +4,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.Gson;
+
 import ch.uzh.marugoto.backend.data.entity.Page;
 import ch.uzh.marugoto.backend.service.PageService;
 
 @RestController
 public class PageController extends BaseController {
-	
+
 	@Autowired
 	private PageService pageService;
 
 	@RequestMapping("/getPages")
 	public String getPages() {
 		Iterable<Page> pages = this.pageService.getAllPages();
-		String json = "";
-		for(Page page : pages) {
-			json = json + page.toString();
+		String json = "[";
+		Gson gson = new Gson();
+		for (Page page : pages) {
+			json = json + gson.toJson(page) + ",";
 		}
+		json = json + "]";
 		return json;
 	}
 }
