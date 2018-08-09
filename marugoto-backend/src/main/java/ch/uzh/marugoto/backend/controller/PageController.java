0c2441d8b0474c3dd342a3870688a6ee4d08c173
Christian Bernet <bernet@vitmain2.ch>
2018-08-09 13:24:50 +0200

    moved to jackson json-encoder from spring (removed gson)

--- marugoto-backend/src/main/java/ch/uzh/marugoto/backend/controller/PageController.java
+++ marugoto-backend/src/main/java/ch/uzh/marugoto/backend/controller/PageController.java
@@ -2,9 +2,8 @@ package ch.uzh.marugoto.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.google.gson.Gson;
 
 import ch.uzh.marugoto.backend.data.entity.Page;
 import ch.uzh.marugoto.backend.service.PageService;
@@ -16,14 +15,14 @@ public class PageController extends BaseController {
 	private PageService pageService;
 
 	@RequestMapping("/getPages")
-	public String getPages() {
+	public Iterable<Page> getPages() {
 		Iterable<Page> pages = this.pageService.getAllPages();
-		String json = "[";
-		Gson gson = new Gson();
-		for (Page page : pages) {
-			json = json + gson.toJson(page) + ",";
-		}
-		json = json + "]";
-		return json;
+		return pages;
+	}
+	
+	@RequestMapping("/getPage")
+	public Page getPage(@RequestParam(value="id") String id) {
+		Page page = this.pageService.getPage(id);
+		return page;
 	}
 }
