2dce252b35f487e6d18bf309d865123e0cdf6c7e
Christian Bernet <bernet@vitmain2.ch>
2018-08-16 14:54:09 +0200

    added page transition to the pagecontroller

--- marugoto-backend/src/main/java/ch/uzh/marugoto/backend/service/PageService.java
+++ marugoto-backend/src/main/java/ch/uzh/marugoto/backend/service/PageService.java
@@ -1,9 +1,14 @@
 package ch.uzh.marugoto.backend.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import ch.uzh.marugoto.backend.data.entity.Page;
+import ch.uzh.marugoto.backend.data.entity.PageTransition;
 import ch.uzh.marugoto.backend.data.repository.PageRepository;
+import ch.uzh.marugoto.backend.data.repository.PageTransitionRepository;
 
 /**
  * PageService assamble the page and pageTransitions as needed by the api. And it holds the business logic.
@@ -14,6 +19,8 @@ public class PageService {
 
 	@Autowired
 	private PageRepository pageRepository;
+	@Autowired
+	private PageTransitionRepository pageTransitionRepository;
 
 
 	public Iterable<Page> getAllPages() {
@@ -31,4 +38,16 @@ public class PageService {
 		Page page = pageRepository.findById("page/" + id).get();
 		return page;
 	}
+	
+	/**
+	 * Get pageTranistions for a page
+	 * 
+	 * @param id
+	 * @return List<PageTransition>
+	 */
+	public List<PageTransition> getPageTransitions(String id) {
+		List<PageTransition> pageTransitions = pageTransitionRepository.getPageTransitionsByPageId("page/" + id);
+		return pageTransitions;
+	}
+
 }
