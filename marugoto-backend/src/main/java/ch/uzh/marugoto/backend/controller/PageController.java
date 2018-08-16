2a78faf04bcfa7f2107c1681c5296c0d07548774
Rino Beeli <rino.beeli@gmail.com>
2018-08-16 13:31:55 +0200

    Global exception handler - proper JSON messages - extensive exception information if not in production mode

--- marugoto-backend/src/main/java/ch/uzh/marugoto/backend/controller/PageController.java
+++ marugoto-backend/src/main/java/ch/uzh/marugoto/backend/controller/PageController.java
@@ -3,6 +3,7 @@ package ch.uzh.marugoto.backend.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ch.uzh.marugoto.backend.data.entity.Page;
 import ch.uzh.marugoto.backend.service.PageService;
@@ -10,6 +11,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 
 @RestController
+@RequestMapping("pages")
 public class PageController extends BaseController {
 
 	@Autowired
@@ -17,16 +19,15 @@ public class PageController extends BaseController {
 	
 
 	@ApiOperation("Load all available pages.")
-	@GetMapping("/pages/list")
+	@GetMapping("list")
 	public Iterable<Page> getPages() {
 		Iterable<Page> pages = this.pageService.getAllPages();
 		return pages;
 	}
 	
 	@ApiOperation("Load page by ID.")
-	@GetMapping("/pages/{id}")
-	public Page getPage(
-			@ApiParam("ID of page.") @PathVariable String id) {
+	@GetMapping("{id}")
+	public Page getPage(@ApiParam("ID of page.") @PathVariable String id) {
 		Page page = this.pageService.getPage(id);
 		return page;
 	}
