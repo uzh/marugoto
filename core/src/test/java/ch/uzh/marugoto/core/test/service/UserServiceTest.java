1f7265751ac2180e11120952c0f1075868d3d6a6
Christian Bernet <bernet@vitmain2.ch>
2018-08-29 11:09:50 +0200

    removed unneeded imports

--- core/src/test/java/ch/uzh/marugoto/core/test/service/UserServiceTest.java
+++ core/src/test/java/ch/uzh/marugoto/core/test/service/UserServiceTest.java
@@ -2,22 +2,22 @@ package ch.uzh.marugoto.core.test.service;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
-
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import com.google.common.collect.Lists;
 
 import ch.uzh.marugoto.core.data.entity.Salutation;
 import ch.uzh.marugoto.core.data.entity.User;
-import ch.uzh.marugoto.core.data.entity.UserType;
 import ch.uzh.marugoto.core.data.repository.UserRepository;
 import ch.uzh.marugoto.core.test.BaseCoreTest;
 
+/**
+ * Simple tests for the UserService class
+ *
+ */
 public class UserServiceTest extends BaseCoreTest {
 
 	
