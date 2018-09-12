160a6aff2e9a1de9cc03403118a16f9c3fca0405
Christian Bernet <bernet@vitmain2.ch>
2018-09-12 17:30:35 +0200

    changed timeout to 4 houres

--- backend/src/main/java/ch/uzh/marugoto/backend/security/Constants.java
+++ backend/src/main/java/ch/uzh/marugoto/backend/security/Constants.java
@@ -3,7 +3,7 @@ package ch.uzh.marugoto.backend.security;
 import java.time.Duration;
 
 public class Constants {
-    public static final long ACCESS_TOKEN_VALIDITY_MS = Duration.ofHours(1).toMillis();
+    public static final long ACCESS_TOKEN_VALIDITY_MS = Duration.ofHours(4).toMillis();
     public static final String SIGNING_KEY = "938jf983fjioejf09834ujfd2x23r2904u430";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
