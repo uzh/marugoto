c5e16f0a06029850d4d8caa61cd11cc9696431c5
Nemanja Tisma <tisma@vitamin2.ch>
2018-10-17 11:21:47 +0200

    Cors filter fix for preflights

--- backend/src/main/java/ch/uzh/marugoto/backend/security/CorsFilter.java
+++ backend/src/main/java/ch/uzh/marugoto/backend/security/CorsFilter.java
@@ -29,9 +29,13 @@ public class CorsFilter implements Filter {
 	        response.setHeader("Access-Control-Allow-Headers", "*");
 	        response.setHeader("Access-Control-Allow-Credentials", "true");
 	        response.setIntHeader("Access-Control-Max-Age", 180);
+
+            if ("OPTIONS".equals(request.getMethod())) {
+                response.setStatus(HttpServletResponse.SC_OK);
+            } else {
+                filterChain.doFilter(request, response);
+            }
         }
-        
-        filterChain.doFilter(servletRequest, servletResponse);
     }
 
     @Override
