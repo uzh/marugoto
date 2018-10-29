64aa5af96cbbb0887e4507eb538ae49e5d217d95
Dusan Tomic <tomic@vitamin2.ch>
2018-10-29 11:19:05 +0100

    moved SMTP configuration

--- core/src/main/java/ch/uzh/marugoto/core/CoreConfiguration.java
+++ core/src/main/java/ch/uzh/marugoto/core/CoreConfiguration.java
@@ -1,9 +1,11 @@
 package ch.uzh.marugoto.core;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,21 +15,19 @@ import com.arangodb.springframework.annotation.EnableArangoRepositories;
 @Configuration
 @ComponentScan(basePackages = { "ch.uzh.marugoto" })
 @EntityScan(basePackages = { "ch.uzh.marugoto" })
+@PropertySource("application-production.properties")
 @EnableArangoRepositories(basePackages = { "ch.uzh.marugoto" })
 public class CoreConfiguration {
 
-//	@Value("${smtp.host}")
-//	private String smtpHost;
-//	
-//	@Value("${smtp.port}")
-//	private int smtpPort;
-//	
-//	@Value("${smtp.username}")
-//	private String smtpUsername;
-//	
-//	@Value("${smtp.password}")
-//	private String smtpPassword;
-//	
+	@Value("${smtp.host}")
+	private String smtpHost;
+	@Value("${smtp.port}")
+	private int smtpPort;
+	@Value("${smtp.username}")
+	private String smtpUsername;
+	@Value("${smtp.password}")
+	private String smtpPassword;
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
@@ -37,15 +37,10 @@ public class CoreConfiguration {
     public JavaMailSenderImpl mailSender() {
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
         
-//        javaMailSender.setHost(smtpHost);
-//        javaMailSender.setPort(smtpPort);
-//        javaMailSender.setUsername(smtpUsername);
-//        javaMailSender.setPassword(smtpPassword);
-        javaMailSender.setHost("smtp.mailtrap.io");
-        javaMailSender.setPort(2525);
-        javaMailSender.setUsername("4b458884e3ec48");
-        javaMailSender.setPassword("dde2d81e4e3f56");
-
+        javaMailSender.setHost(smtpHost);
+        javaMailSender.setPort(smtpPort);
+        javaMailSender.setUsername(smtpUsername);
+        javaMailSender.setPassword(smtpPassword);
         return javaMailSender;
     }
 }
