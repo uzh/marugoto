7a0a2cf101e62025428e2c707d441c5c283b3dff
Nemanja Tisma <tisma@vitamin2.ch>
2018-11-18 22:20:31 +0100

    changed folder structure

--- shell/src/main/java/ch/uzh/marugoto/shell/commands/GenerateTemplatesCommand.java
+++ shell/src/main/java/ch/uzh/marugoto/shell/commands/GenerateTemplatesCommand.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.checkerframework.checker.units.qual.A;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.util.StringUtils;
@@ -20,10 +22,26 @@ import ch.uzh.marugoto.core.data.entity.Storyline;
 import ch.uzh.marugoto.core.data.entity.TextComponent;
 import ch.uzh.marugoto.core.data.entity.TextExercise;
 import ch.uzh.marugoto.core.data.entity.Topic;
+import ch.uzh.marugoto.core.data.repository.ComponentRepository;
+import ch.uzh.marugoto.core.data.repository.PageRepository;
+import ch.uzh.marugoto.core.data.repository.PageTransitionRepository;
+import ch.uzh.marugoto.core.data.repository.StorylineRepository;
+import ch.uzh.marugoto.core.data.repository.TopicRepository;
 import ch.uzh.marugoto.shell.util.FileGenerator;
 
 @ShellComponent
 public class GenerateTemplatesCommand {
+
+	@Autowired
+	private TopicRepository topicRepository;
+	@Autowired
+	private StorylineRepository storylineRepository;
+	@Autowired
+	private PageRepository pageRepository;
+	@Autowired
+	private PageTransitionRepository pageTransitionRepository;
+	@Autowired
+	private ComponentRepository componentRepository;
 
 	@ShellMethod("Generates folder structure and empty json files")
 	public void generateTemplateFiles(String destinationPath) {
@@ -31,26 +49,39 @@ public class GenerateTemplatesCommand {
 		var templatesFolder = FileGenerator.generateFolder(destinationPath);
 
 		// TOPIC
-		FileGenerator.generateJsonFilesFromObject(new Topic(), 1, "topic", templatesFolder);
-		// STORY LINES
-		var storylineFolder = FileGenerator.generateFolder(templatesFolder.getPath() + File.separator + "storylines");
-		FileGenerator.generateJsonFilesFromObject(new Storyline(null, false), 1, "storyline", storylineFolder);
+		FileGenerator.generateJsonFileFromObject(new Topic(), "topic", templatesFolder);
+
+		// STORY LINE
+		var storylineFolder = FileGenerator.generateFolder(templatesFolder.getPath(), "storyline");
+		FileGenerator.generateJsonFileFromObject(new Storyline(null, false), "storyline", storylineFolder);
 
 		// PAGES
-		var pagesFolder = FileGenerator.generateFolder(templatesFolder.getPath() + File.separator + "pages");
-		FileGenerator.generateJsonFilesFromObject(new Page(), 4, "page", pagesFolder);
+		var pagesFolder = FileGenerator.generateFolder(storylineFolder.getPath(), "page", 4);
 
-		// PAGE TRANSITIONS
-		var pageTransitionsFolder = FileGenerator.generateFolder(templatesFolder.getPath() + File.separator + "pageTransitions");
-		FileGenerator.generateJsonFilesFromObject(new PageTransition(null, null, null), 5, "pageTransition", pageTransitionsFolder);
+		var page = new Page();
+		var pageTransition = new PageTransition(null, null, null);
+		var textComponent = new TextComponent(0, null);
 
-		// COMPONENTS
-		var componentsFolder = FileGenerator.generateFolder(templatesFolder.getPath() + File.separator + "components");
-		// Text components
-		FileGenerator.generateJsonFilesFromObject(new TextComponent(0, null), 4, "textComponent", componentsFolder);
-		// Text exercise
-		FileGenerator.generateJsonFilesFromObject(new TextExercise(0, 0, 0, null), 1, "textExercise", componentsFolder);
-		// Radio button exercise
-		FileGenerator.generateJsonFilesFromObject(new RadioButtonExercise(0, null, null), 1, "radioButtonExercise", componentsFolder);
+		// add page json to each page folder
+		for (int i = 0; i < pagesFolder.size(); i++) {
+			FileGenerator.generateJsonFileFromObject(page, "page", pagesFolder.get(i));
+		}
+
+		var page1 = pagesFolder.get(0);
+		FileGenerator.generateJsonFileFromObject(pageTransition, "pageTransition", page1);
+		FileGenerator.generateJsonFileFromObject(textComponent, "textComponent", page1);
+
+		var page2 = pagesFolder.get(1);
+		FileGenerator.generateJsonFileFromObject(pageTransition, "pageTransition", page2);
+		FileGenerator.generateJsonFileFromObject(textComponent, "textComponent", page2);
+
+		var page3 = pagesFolder.get(2);
+		FileGenerator.generateJsonFileFromObject(pageTransition, "pageTransition", page3, 3);
+		FileGenerator.generateJsonFileFromObject(textComponent, "textComponent", page3);
+		FileGenerator.generateJsonFileFromObject(new TextExercise(0, 0, 0, null), "textExercise", page3);
+		FileGenerator.generateJsonFileFromObject(new RadioButtonExercise(0, null, null), "radioButtonExercise", page3);
+
+		var page4 = pagesFolder.get(3);
+		FileGenerator.generateJsonFileFromObject(textComponent, "textComponent", page4);
 	}
 }
--- shell/src/main/java/ch/uzh/marugoto/shell/util/FileGenerator.java
+++ shell/src/main/java/ch/uzh/marugoto/shell/util/FileGenerator.java
@@ -7,6 +7,8 @@ import org.springframework.util.StringUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class FileGenerator {
@@ -15,8 +17,14 @@ public class FileGenerator {
             .disable(MapperFeature.USE_ANNOTATIONS)
             .disable(MapperFeature.ALLOW_EXPLICIT_PROPERTY_RENAMING);
 
-    public static File generateFolder(String path) {
-        File folder = new File(path);
+    /**
+     * Generates folder from provided path
+     *
+     * @param destinationPath
+     * @return
+     */
+    public static File generateFolder(String destinationPath) {
+        File folder = new File(destinationPath);
 
         if (!folder.exists()) {
             folder.mkdirs();
@@ -25,18 +33,52 @@ public class FileGenerator {
         return folder;
     }
 
-    public static void generateJsonFilesFromObject(Object object, int numOfFiles, String fileName, File destinationFolder) {
+    /**
+     * Makes folder with provided name on the provided path
+     *
+     * @param destinationPath
+     * @param folderName
+     * @return
+     */
+    public static File generateFolder(String destinationPath, String folderName) {
+        return generateFolder(destinationPath + File.separator + folderName);
+    }
 
+    /**
+     * Makes multiple folders with provided name on the provided path
+     * folders will have suffix (1,2,3...)
+     *
+     * @param destinationPath
+     * @param folderName
+     * @param numberOfFolders
+     * @return
+     */
+    public static List<File> generateFolder(String destinationPath, String folderName, int numberOfFolders) {
+        List<File> generatedFolders = new ArrayList<>();
+
+        for (int i = 1; i <= numberOfFolders; i++) {
+            var name = folderName + i;
+            generatedFolders.add(generateFolder(destinationPath, name));
+        }
+
+        return generatedFolders;
+    }
+
+    public static void generateJsonFileFromObject(Object object, String fileName, File destinationFolder, int numOfFiles) {
         for (int i = 1; i <= numOfFiles; i++) {
-            var jsonFileName = fileName + i + ".json";
-            var json = new File(destinationFolder.getPath() + File.separator + jsonFileName);
+            var jsonFileName = fileName + i;
+            generateJsonFileFromObject(object, jsonFileName, destinationFolder);
+        }
+    }
 
-            try {
-                mapper.writeValue(json, object);
-                System.out.println(jsonFileName + " added to " + destinationFolder.getName() + " (total " + Objects.requireNonNull(destinationFolder.listFiles()).length + ")");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+    public static void generateJsonFileFromObject(Object object, String fileName, File destinationFolder) {
+        var json = new File(destinationFolder.getPath() + File.separator + fileName  + ".json");
+
+        try {
+            mapper.writeValue(json, object);
+            System.out.println(fileName + " added to " + destinationFolder.getName() + " (total " + Objects.requireNonNull(destinationFolder.listFiles()).length + ")");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
