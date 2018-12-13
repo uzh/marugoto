ad0f91a807c8a94f5583aa6247835f5f470149f9
Nemanja Tisma <tisma@vitamin2.ch>
2018-12-13 12:04:56 +0100

    Fixed failing date exercise test

--- core/src/test/java/ch/uzh/marugoto/core/test/service/ExerciseServiceTest.java
+++ core/src/test/java/ch/uzh/marugoto/core/test/service/ExerciseServiceTest.java
@@ -91,7 +91,7 @@ public class ExerciseServiceTest extends BaseCoreTest {
 
     @Test
     public void testDateExercise () {
-        String time = "06.12.2018";
+        String time = "02.02.2002";
         var dateExercise = exerciseService.getExercises(page4)
                 .stream()
                 .filter(exercise -> exercise instanceof DateExercise)
