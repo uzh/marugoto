8b29480702dfcba118c097e4bea4e13c2eb0aae6
Nemanja Tisma <tisma@vitamin2.ch>
2019-01-16 10:04:53 +0100

    Fixed building war file

--- backend/src/main/java/ch/uzh/marugoto/backend/BackendApplication.java
+++ backend/src/main/java/ch/uzh/marugoto/backend/BackendApplication.java
@@ -1,15 +1,10 @@
 package ch.uzh.marugoto.backend;
 
-import com.arangodb.springframework.core.ArangoOperations;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -24,8 +19,7 @@ import ch.uzh.marugoto.core.CoreConfiguration;
  */
 @SpringBootApplication
 @Import(CoreConfiguration.class)
-public class BackendApplication implements ApplicationRunner {
-	private static final Logger logger = LogManager.getLogger(BackendApplication.class);
+public class BackendApplication extends SpringBootServletInitializer {
 
 	@Value("${marugoto.database}")
 	private String DB_NAME;
@@ -33,53 +27,22 @@ public class BackendApplication implements ApplicationRunner {
 	@Value("${spring.profiles.active}")
 	private String SPRING_PROFILE;
 
-	@Autowired
-	private ArangoOperations operations;
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BackendApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
-	@Override
-	public void run(ApplicationArguments applicationArguments) {
-		logger.info("------------------------------------------------");
-		logger.info("Marugoto backend started:");
-		logger.info(" > Spring profile: " + SPRING_PROFILE);
-		logger.info(" > Java version: " + System.getProperty("java.version"));
-		logger.info(" > Database: " + DB_NAME);
-
-		// Make sure database exists, create if not
-		if (!operations.driver().getDatabases().contains(DB_NAME)) {
-			operations.driver().createDatabase(DB_NAME);
-			logger.info(String.format("Database `%s` created.", DB_NAME));
-		}
-		//check if every collection is added
-		operations.collection("chapter");
-		operations.collection("character");
-		operations.collection("component");
-		operations.collection("dialogResponse");
-		operations.collection("dialogSpeech");
-		operations.collection("exerciseState");
-		operations.collection("notebookEntry");
-		operations.collection("page");
-		operations.collection("pageState");
-		operations.collection("pageTransition");
-		operations.collection("personalNote");
-		operations.collection("resource");
-		operations.collection("storyline");
-		operations.collection("storylineState");
-		operations.collection("topic");
-		operations.collection("user");
-		logger.info("------------------------------------------------");
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
-	
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-    
-    @Bean
-    public MethodValidationPostProcessor methodValidationPostProcessor() {
-         return new MethodValidationPostProcessor();
-    }
+
+	@Bean
+	public MethodValidationPostProcessor methodValidationPostProcessor() {
+		return new MethodValidationPostProcessor();
+	}
 }
