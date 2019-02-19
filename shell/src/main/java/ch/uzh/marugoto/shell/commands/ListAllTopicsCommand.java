2b1b90e307b99b55f1050a36b22890b48d22c5c9
Dusan Tomic <tomic@vitamin2.ch>
2019-02-19 13:19:51 +0100

    Minor changes in topic list command

--- shell/src/main/java/ch/uzh/marugoto/shell/commands/ListAllTopicsCommand.java
+++ shell/src/main/java/ch/uzh/marugoto/shell/commands/ListAllTopicsCommand.java
@@ -1,14 +1,9 @@
 package ch.uzh.marugoto.shell.commands;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 
-import com.google.common.collect.Lists;
-
-import ch.uzh.marugoto.core.data.entity.topic.Topic;
 import ch.uzh.marugoto.core.data.repository.TopicRepository;
 
 @ShellComponent
@@ -20,11 +15,11 @@ public class ListAllTopicsCommand {
 	@ShellMethod("List all topics")
 	public void listTopics() {
 		
-		List<Topic>topics = Lists.newArrayList(topicRepository.findAll().iterator());
+		var topics = topicRepository.findAll().iterator();
 		System.out.println("TOPICS:\n");
-		topics.stream().forEach(topic -> {
-			System.out.println("title: " +topic.getTitle()+ "\n "+"id: "+topic.getId()+ "\r active: "+ topic.isActive());
-			System.out.println("\n");
-		});
+		while(topics.hasNext()){
+			var topic = topics.next();
+			System.out.println("title: " +topic.getTitle()+ "\n "+"id: "+topic.getId()+ "\r active: "+ topic.isActive()+"\n");
+		}
 	}
 }
