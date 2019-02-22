be604f7750e1d71abe2a806eeea00fbff3273378
Nemanja Tisma <tisma@vitamin2.ch>
2019-02-22 09:54:12 +0100

    Removed not needed method

--- core/src/main/java/ch/uzh/marugoto/core/helpers/DtoHelper.java
+++ core/src/main/java/ch/uzh/marugoto/core/helpers/DtoHelper.java
@@ -7,16 +7,6 @@ import ch.uzh.marugoto.core.exception.DtoToEntityException;
 
 public class DtoHelper {
 
-    public static Object convertToEntity(RequestDto dtoObject, Class<?> clazz) throws DtoToEntityException {
-        try {
-            var objectInstance = clazz.getDeclaredConstructor().newInstance();
-            BeanUtils.copyProperties(dtoObject, objectInstance);
-            return objectInstance;
-        } catch (Exception e) {
-            throw new DtoToEntityException();
-        }
-    }
-
     public static void map(RequestDto dtoObject, Object mapToObject) throws DtoToEntityException {
         try {
             BeanUtils.copyProperties(dtoObject, mapToObject);
