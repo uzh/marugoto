be597253eb5d7820d923578efdefa8b29512249a
Christian Bernet <>
2019-04-25 15:16:12 +0200

    Review importerId

--- shell/src/main/java/ch/uzh/marugoto/shell/commands/DatabaseCommand.java
+++ shell/src/main/java/ch/uzh/marugoto/shell/commands/DatabaseCommand.java
@@ -23,112 +23,109 @@ import ch.uzh.marugoto.shell.util.Importer;
 @ShellComponent
 public class DatabaseCommand {
 
+	@Value("${marugoto.database}")
+	private String DB_NAME;
+	@Value("${spring.profiles.active}")
+	private String SPRING_PROFILE;
+	@Autowired
+	private ArangoOperations operations;
+	private Importer importer;
 
-    @Value("${marugoto.database}")
-    private String DB_NAME;
-    @Value("${spring.profiles.active}")
-    private String SPRING_PROFILE;
-    @Autowired
-    private ArangoOperations operations;
-    private Importer importer;
+	@ShellMethod("Truncate Database")
+	public void truncateDatabase() {
+		var dbConfig = BeanUtil.getBean(DbConfiguration.class);
+		var operations = BeanUtil.getBean(ArangoOperations.class);
 
-    
-    @ShellMethod("Truncate Database")
-    public void truncateDatabase() {
-        var dbConfig = BeanUtil.getBean(DbConfiguration.class);
-        var operations = BeanUtil.getBean(ArangoOperations.class);
+		System.out.println(String.format("Truncating database `%s`...", dbConfig.database()));
 
-        System.out.println(String.format("Truncating database `%s`...", dbConfig.database()));
+		if (operations.driver().getDatabases().contains(dbConfig.database())) {
+			operations.dropDatabase();
+		}
 
-        if (operations.driver().getDatabases().contains(dbConfig.database())) {
-            operations.dropDatabase();
-        }
+		operations.driver().createDatabase(dbConfig.database());
+	}
 
-        operations.driver().createDatabase(dbConfig.database());
-    }
+	@ShellMethod("`/path/to/generated/folder` importerIFolderName. boolean for deleting player state")
+	public void doImport(String pathToDirectory, String importerId, @Nullable String deletePlayerState)
+			throws Exception {
+		System.out.println("Preparing database:  " + DB_NAME);
+		prepareDb();
 
-    @ShellMethod("`/path/to/generated/folder` importerIFolderName. boolean for deleting player state")
-    public void doImport(String pathToDirectory, String importerId, @Nullable String deletePlayerState) throws Exception {
-        System.out.println("Preparing database:  " + DB_NAME);
-        prepareDb();
-        
+		if (System.getProperty("os.name").compareTo("Windows 7") == 0) {
+			pathToDirectory = pathToDirectory.replace("/", "\\");
+		}
+		if (FileHelper.hiddenFolderExist(pathToDirectory, importerId) == true) {
 
-        if(System.getProperty("os.name").compareTo("Windows 7") == 0) {
-            pathToDirectory = pathToDirectory.replace("/", "\\");
-        }
-        if (FileHelper.hiddenFolderExist(pathToDirectory,importerId) == true) {
-        	
-        	var foldersAreTheSame = FileHelper.compareFolders(pathToDirectory, FileHelper.getPathToImporterFolder(pathToDirectory,importerId));
-        	
-        	if (foldersAreTheSame == true) {
-        		importer = new ImportUpdate(pathToDirectory,importerId);
-        	}
-        	else {
-        		System.out.println("WARNING! You are about to remove player state. " 
-        				+ "If you want to procced, please run the command again with the flag ```delete.playerState``` setted to true" );
-        		if (deletePlayerState.toLowerCase().equals(Boolean.TRUE.toString())) {
-        			System.out.println("deletePlayerState is currently: " + deletePlayerState);
-        			importer = new ImportOverride(pathToDirectory,importerId);
-        		}
-        	}
-        	
-        } else {
-        	importer = new ImportInsert(pathToDirectory,importerId);
-        }
-        
-       importer.doImport();
-        // we need this for states collections
-        createMissingCollections();
+			boolean foldersAreTheSame = FileHelper.compareFolders(pathToDirectory,
+					FileHelper.getPathToImporterFolder(pathToDirectory, importerId));
+			if (foldersAreTheSame == true) {
+				importer = new ImportUpdate(pathToDirectory, importerId);
+			} else {
+				System.out.println("WARNING! You are about to remove player state. "
+						+ "If you want to procced, please run the command again with the flag ```deletePlayerState``` setted to true");
+				if (deletePlayerState.toLowerCase().equals(Boolean.TRUE.toString())) {
+					System.out.println("deletePlayerState is currently: " + deletePlayerState);
+					importer = new ImportOverride(pathToDirectory, importerId);
+				}
+			}
 
-        System.out.println("Finished");
-    }
-    
-    @ShellMethod("Create missing collections")
-    public void createMissingCollections() {
-        //check if every collection is added
-        operations.collection("chapter");
-        operations.collection("character");
-        operations.collection("component");
-        operations.collection("notification");
-        operations.collection("mailState");
-        operations.collection("dialogState");
-        operations.collection("dialogResponse", new CollectionCreateOptions().type(CollectionType.EDGES));
-        operations.collection("dialogSpeech");
-        operations.collection("exerciseState");
-        operations.collection("notebookEntry");
-        operations.collection("notebookEntryState");
-        operations.collection("notebookContent");
-        operations.collection("page");
-        operations.collection("pageState");
-        operations.collection("pageTransition", new CollectionCreateOptions().type(CollectionType.EDGES));
-        operations.collection("personalNote");
-        operations.collection("resource");
-        operations.collection("gameState");
-        operations.collection("topic");
-        operations.collection("user");
-        operations.collection("classroom");
-        operations.collection("classroomMember", new CollectionCreateOptions().type(CollectionType.EDGES));
-    }
-    
-    private void prepareDb() {
-        // Make sure database exists, create if not
-        if (!operations.driver().getDatabases().contains(DB_NAME)) {
-            operations.driver().createDatabase(DB_NAME);
-        }
-    }
-    
-    /**
-     * Called on shell container startup
-     * 
-     * @param event
-     * @throws ImporterNotFoundException
-     * @throws Exception
-     */
-    @EventListener(ContextRefreshedEvent.class)
+		} else {
+			importer = new ImportInsert(pathToDirectory, importerId);
+		}
+
+		importer.doImport();
+		// we need this for states collections
+		createMissingCollections();
+
+		System.out.println("Finished");
+	}
+
+	@ShellMethod("Create missing collections")
+	public void createMissingCollections() {
+		// check if every collection is added
+		operations.collection("chapter");
+		operations.collection("character");
+		operations.collection("component");
+		operations.collection("notification");
+		operations.collection("mailState");
+		operations.collection("dialogState");
+		operations.collection("dialogResponse", new CollectionCreateOptions().type(CollectionType.EDGES));
+		operations.collection("dialogSpeech");
+		operations.collection("exerciseState");
+		operations.collection("notebookEntry");
+		operations.collection("notebookEntryState");
+		operations.collection("notebookContent");
+		operations.collection("page");
+		operations.collection("pageState");
+		operations.collection("pageTransition", new CollectionCreateOptions().type(CollectionType.EDGES));
+		operations.collection("personalNote");
+		operations.collection("resource");
+		operations.collection("gameState");
+		operations.collection("topic");
+		operations.collection("user");
+		operations.collection("classroom");
+		operations.collection("classroomMember", new CollectionCreateOptions().type(CollectionType.EDGES));
+	}
+
+	private void prepareDb() {
+		// Make sure database exists, create if not
+		if (!operations.driver().getDatabases().contains(DB_NAME)) {
+			operations.driver().createDatabase(DB_NAME);
+		}
+	}
+
+	/**
+	 * Called on shell container startup
+	 * 
+	 * @param event
+	 * @throws ImporterNotFoundException
+	 * @throws Exception
+	 */
+	@EventListener(ContextRefreshedEvent.class)
 	public void contextRefreshedEvent(ContextRefreshedEvent event) throws Exception {
 //    	System.out.println("Path is: " + path);
-    //	if (!path.isEmpty()) {
-    //		doImport(path);
-    //	}
+		// if (!path.isEmpty()) {
+		// doImport(path);
+		// }
 	}
 }
