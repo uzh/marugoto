e837698a29c35aa9da90354999033f50d1a25098
Christian Bernet <>
2019-05-09 14:49:30 +0200

    changed typo shell command description

--- shell/src/main/java/ch/uzh/marugoto/shell/commands/DatabaseCommand.java
+++ shell/src/main/java/ch/uzh/marugoto/shell/commands/DatabaseCommand.java
@@ -51,7 +51,7 @@ public class DatabaseCommand {
 		operations.driver().createDatabase(dbConfig.database());
 	}
 
-	@ShellMethod("`/path/to/generated/folder` importerIFolderName. boolean for deleting player state")
+	@ShellMethod("`/path/to/generated/folder` importerFolderName. boolean for deleting player state")
 	public void doImport(String pathToDirectory, String importerId,
 			@ShellOption(defaultValue = "false") String deletePlayerState) throws Exception {
 		System.out.println("Preparing database:  " + DB_NAME);
