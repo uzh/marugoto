961564cedc6b83d00ae00fcf257aeaabc55358c3
Dusan Tomic <tomic@vitamin2.ch>
2019-05-15 13:19:40 +0200

    removed warnings and unused method

--- backend/src/main/java/ch/uzh/marugoto/backend/controller/GameController.java
+++ backend/src/main/java/ch/uzh/marugoto/backend/controller/GameController.java
@@ -1,33 +1,23 @@
 package ch.uzh.marugoto.backend.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.List;
 
 import javax.naming.AuthenticationException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 import ch.uzh.marugoto.core.data.entity.application.User;
 import ch.uzh.marugoto.core.data.entity.state.GameState;
-import ch.uzh.marugoto.core.exception.CreatePdfException;
-import ch.uzh.marugoto.core.exception.CreateZipException;
-import ch.uzh.marugoto.core.exception.DownloadNotebookException;
 import ch.uzh.marugoto.core.exception.GameStateNotInitializedException;
 import ch.uzh.marugoto.core.service.GameStateService;
 import ch.uzh.marugoto.core.service.StateService;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.Authorization;
 
 @RestController
@@ -65,23 +55,5 @@ public class GameController extends BaseController {
         gameStateService.setGameState("gameState/".concat(gameStateId), user);
         return stateService.getStates(user);
     }
-    
-    /**
-//     * Download compressed file with students notebook and files for specific topic
-//     * @return zip file files.zip
-//     */
-//    @ApiOperation(value = "Download compressed file with students notebook and uploaded files within a class.", authorizations = { @Authorization(value = "apiKey")})
-//    @GetMapping(value = "{classId}/files", produces = "application/zip")
-//
-//    public ResponseEntity<InputStreamResource> downloadNotebooks(@PathVariable String studentId, @ApiParam("Id of topic") @RequestParam("topicId") String topicId) throws AuthenticationException, CreateZipException {
-//    
-//    	
-//    	FileInputStream zip = notebookService.getClassroomNotebooks(students, classId);
-//        InputStreamResource streamResource = new InputStreamResource(zip);
-//
-//        log.info(String.format("%s has downloaded notebooks zip file for classroom ID %s", getAuthenticatedUser().getName(), classId));
-//
-//        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=notebooks.zip").body(streamResource);
-//    }
     
 }
