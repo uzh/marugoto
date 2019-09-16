69e831aace35f8d949c84afd5fb13f1975332526
Christian Bernet <>
2019-09-16 13:40:33 +0200

    removed warning

--- backend/src/main/java/ch/uzh/marugoto/backend/controller/GameController.java
+++ backend/src/main/java/ch/uzh/marugoto/backend/controller/GameController.java
@@ -10,7 +10,6 @@ import javax.naming.AuthenticationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
